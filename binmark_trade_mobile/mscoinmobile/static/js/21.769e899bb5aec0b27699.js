e7686b376442d4bd54401ac4b2dd339393d63ae6
陈玖林 <531502166@qq.com>
2019-01-30 21:34:40 +0800

    打包

---
+++ binmark_trade_mobile/mscoinmobile/static/js/21.769e899bb5aec0b27699.js
@@ -0,0 +1,2 @@
+webpackJsonp([21],{RKbU:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"news-home wh-full app-main"},[t("my-header"),this._v(" "),t("div",{staticClass:"van-hairline--top"}),this._v(" "),t("router-view")],1)},staticRenderFns:[]};var i=n("C7Lr")(null,a,!1,function(e){n("nkJZ")},"data-v-72b06e07",null);t.default=i.exports},nkJZ:function(e,t){}});
+//# sourceMappingURL=21.769e899bb5aec0b27699.js.map
